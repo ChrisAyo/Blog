88d2338cd9bfcb4033d1bff7498ab1434c55c1fc
ChrisAyo <Chris.ayodele01@gmail.com>
2023-12-15 11:16:48 +0000

    WIP Change navbarlinks text white

--- layouts/default.vue
+++ layouts/default.vue
@@ -10,13 +10,13 @@
         <ul class="navbar-section" @click="closeNavMenu()">
           <!-- <v-app-bar-nav-icon @click.stop="drawer = !drawer" /> -->
           <NuxtLink to="/">
-            <v-toolbar-title class="red--text">Home</v-toolbar-title>
+            <v-toolbar-title class="white--text">Home</v-toolbar-title>
           </NuxtLink>
           <NuxtLink to="/about">
-            <v-toolbar-title class="red--text">About</v-toolbar-title>
+            <v-toolbar-title class="white--text">About</v-toolbar-title>
           </NuxtLink>
           <NuxtLink to="/my-first-blog-post">
-            <v-toolbar-title class="red--text">Explore</v-toolbar-title>
+            <v-toolbar-title class="white--text">Explore</v-toolbar-title>
           </NuxtLink>
         </ul>
         <div class="hamburger" @click="burgerOnClick()">
